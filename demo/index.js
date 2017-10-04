85a8dc0147caaaaf8b80370294e038b5d8ed962f
Sean Dunn <sedunn@adobe.com>
2017-10-04 12:58:06 -0400

    crop: Add scaled dimensions to crop data

--- demo/index.js
+++ demo/index.js
@@ -1,66 +1,62 @@
-define([
-  'jquery',
-  '../src/js/index',
-], function($, Helicropter) {
-  'use strict';
+import $ from 'jquery';
+import Helicropter from '../src/js/index';
 
-  var cropper = new Helicropter({
-    uploaderOptions: {
-      request: {
-        endpoint: 'foo',
-        accessKey: 'foo',
-      },
-      signature: {
-        endpoint: '/s3handler',
-      },
+var cropper = new Helicropter({
+  uploaderOptions: {
+    request: {
+      endpoint: 'foo',
+      accessKey: 'foo',
     },
-    canvasSize: {
-      width: 576,
-      height: 362,
+    signature: {
+      endpoint: '/s3handler',
     },
-    cropSize: {
-      width: 404,
-      height: 316,
+  },
+  canvasSize: {
+    width: 576,
+    height: 362,
+  },
+  cropSize: {
+    width: 404,
+    height: 316,
+  },
+  displayedWidth: 500,
+  previewCrop: {
+    element: $('.js-preview-crop-container'),
+  },
+  cropRatio: {
+    width: 4,
+    height: 3,
+  },
+  viewportRatio: 'static',
+  allowTransparency: false,
+  // uploadTitle: 'Upload a new cover image',
+  // uploadSubtitle: 'This will not affect your Behance cover image',
+  initialImage: {
+    src: '/imgs/test-letters.png',
+    url: 'https://foo.com/imgs/test-letters.png',
+    coordinates: {
+      x: 48,
+      y: 65,
+      scale: 0.4105,
     },
-    displayedWidth: 500,
-    previewCrop: {
-      element: $('.js-preview-crop-container'),
-    },
-    cropRatio: {
-      width: 4,
-      height: 3,
-    },
-    viewportRatio: 'static',
-    allowTransparency: false,
-    // uploadTitle: 'Upload a new cover image',
-    // uploadSubtitle: 'This will not affect your Behance cover image',
-    initialImage: {
-      src: '/imgs/test-letters.png',
-      url: 'https://foo.com/imgs/test-letters.png',
-      coordinates: {
-        x: 48,
-        y: 65,
-        scale: 0.4105,
-      },
-    },
-    // previewMode: true,
-    uploadBackgroundImage: '/imgs/test-letters.png',
-    showRatioLock: true,
-    showSuggestions: true,
-    suggestions: [
-      { src: '/imgs/test-letters.png', url: 'https://foo.com/imgs/test-letters.png', active: true },
-      { src: '/imgs/test-image-0.png', url: 'https://foo.com/imgs/test-image-0.png' },
-      { src: '/imgs/test-image-1.png', url: 'https://foo.com/imgs/test-image-1.png' },
-      { src: '/imgs/test-image-2.png', url: 'https://foo.com/imgs/test-image-2.png' },
-      { src: '/imgs/test-image-3.png', url: 'https://foo.com/imgs/test-image-3.png' },
-    ],
-  });
-  cropper.render($('.js-cropper'));
-
-  cropper.on('error:upload', function(err) {
-    console.error('Error:', err.message);
-  });
-  window.cropper = cropper;
-
-  $('.removeImage').on('click', function() { cropper.removeImage(); });
+  },
+  // previewMode: true,
+  uploadBackgroundImage: '/imgs/test-letters.png',
+  showRatioLock: true,
+  showSuggestions: true,
+  suggestions: [
+    { src: '/imgs/test-letters.png', url: 'https://foo.com/imgs/test-letters.png', active: true },
+    { src: '/imgs/test-image-0.png', url: 'https://foo.com/imgs/test-image-0.png' },
+    { src: '/imgs/test-image-1.png', url: 'https://foo.com/imgs/test-image-1.png' },
+    { src: '/imgs/test-image-2.png', url: 'https://foo.com/imgs/test-image-2.png' },
+    { src: '/imgs/test-image-3.png', url: 'https://foo.com/imgs/test-image-3.png' },
+  ],
 });
+cropper.render($('.js-cropper'));
+
+cropper.on('error:upload', function(err) {
+  console.error('Error:', err.message);
+});
+window.cropper = cropper;
+
+$('.removeImage').on('click', function() { cropper.removeImage(); });
